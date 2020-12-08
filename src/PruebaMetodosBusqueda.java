8447164d72602d74433a0f3b59d84bc42ed43850
Bryan Valdez <bryan501th@gmail.com>
2020-12-08 15:40:26 -0600

    Se agregó el algoritmo de busqueda por Hash

--- src/PruebaMetodosBusqueda.java
+++ src/PruebaMetodosBusqueda.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class BusquedaBinaria {
 
 	public static boolean busquedaBin(int[] numeros, int valorBuscado) {
@@ -23,6 +25,100 @@ class BusquedaBinaria {
 
 }
 
+class FuncionHash {
+
+	private String[] arreglo;
+	private int tamaño;
+	private int contador;
+	
+	public String[] getArreglo(){
+		return this.arreglo;
+	}
+
+	// Constructor
+	public FuncionHash(int tam) {
+		tamaño = tam;
+		arreglo = new String[tam];
+		Arrays.fill(arreglo, "-1");
+	}
+
+	// Funcion HASH
+	public void funcionHash(String[] cadArreglo, String[] arreglo) {
+		int i;
+		// Ciclo para asiganar a la varible elemento el valor de la cadena
+		for (i = 0; i < cadArreglo.length; i++) {
+			String elemento = cadArreglo[i];
+			int indiceArreglo = Integer.parseInt(elemento) % 7;
+			System.out.println("Indice: " + indiceArreglo + " para " + elemento);
+			// Mpetodo para solucionar una colision
+			while (arreglo[indiceArreglo] != "-1") {
+				indiceArreglo++;
+				System.out.println("Colisión en el indice: " + (indiceArreglo - 1) + " cambiando por " + indiceArreglo);
+				// Cambiar al indice siguiente y asi evitar la colision
+				indiceArreglo %= tamaño; // Para volver a sacar el valor
+			}
+			arreglo[indiceArreglo] = elemento;
+		}
+	}
+
+	// Metodo para mostrar la funcion hash
+	public void mostrar() {
+		int incremento = 0;
+		int j;
+
+		for (int i = 0; i < 1; i++) {
+			incremento += 8;
+			for (j = 0; j < 100; j++) {
+			}
+			System.out.println("");
+			System.out.println("------------------------------------------------------------------");
+			for (j = incremento - 8; j < incremento; j++) {
+				System.out.format(" | %3s " + " ", j);
+			}
+			System.out.println(" | ");
+			for (int k = 0; k < 100; k++) {
+			}
+			System.out.println();
+			for (j = incremento - 8; j < incremento; j++) {
+				if (arreglo[j].equals("-1")) {
+					System.out.println(" | ");
+				} else {
+					System.out.print(String.format(" | %3s" + " ", arreglo[j]));
+				}
+			}
+
+			System.out.println("|");
+			System.out.println("------------------------------------------------------------------");
+			for (j = 0; j < 100; j++) {
+			}
+			System.out.println("");
+		}
+	}
+
+	// Metodo para buscar una clave de los elementos
+	public String buscarClave(String elemento) {
+		
+		int indiceArrglo = Integer.parseInt(elemento) % 7;
+		int contador = 0;
+		
+		while (arreglo[indiceArrglo] != "-1") {
+			if (arreglo[indiceArrglo] == elemento) {
+				System.out.println("Elemento " + elemento + " se encontro en el indice" + indiceArrglo);
+				return arreglo[indiceArrglo];
+			}
+			indiceArrglo++;
+			indiceArrglo %= tamaño;
+			contador++;
+			if (contador > 7) {
+				System.out.print("Error");
+				break;
+			}
+		}
+		return null;
+	}
+	
+}
+
 public class PruebaMetodosBusqueda {
 
 	public static void main(String[] args) {
@@ -33,6 +129,21 @@ public class PruebaMetodosBusqueda {
 		}else {
 			System.out.println("no encontrado");
 		}
+		
+		FuncionHash funcion = new FuncionHash(8);
+		String[] elementos= {"25", "14", "96", "300", "1", "12", "8", "15"};
+		funcion.funcionHash(elementos, funcion.getArreglo());
+		funcion.mostrar();
+		
+		String buscarElemento = funcion.buscarClave("12");
+		if(buscarElemento==null) {
+			System.out.println("\nElemento no encontrado");
+		}
+		
+		
+		
+		
+		
 
 	}
 
